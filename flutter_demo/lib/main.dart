fa08c19f51c4e5cea9eac2b272df62cfb002801d
Pradyot Prakash <35835404+pradyotprksh@users.noreply.github.com>
2021-04-18 20:18:26 +0530

    Initial Setup
    - Removed unwanted codes and files
    - Updated .gitignore with the files which are not required in VCS system

---
+++ flutter_demo/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// Main entry of the application.
+void main() {
+  runApp(MyApp());
+}
+
+/// The main MyApp class which contains the initial application details.
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
